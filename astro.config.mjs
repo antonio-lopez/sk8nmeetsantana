fadcaa5f917ca50dab4e1541b776976cda98a5ed
antonio-lopez <36496887+antonio-lopez@users.noreply.github.com>
2024-12-16 10:24:09 -0800

    chore: add astro icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,22 +6,20 @@ import tailwind from "@astrojs/tailwind";
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   image: {
     domains: ["res.cloudinary.com"],
   },
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    sanity({
-      projectId: "jkn6q794",
-      dataset: "production",
-      useCdn: false, // See note on using the CDN
-      apiVersion: "2024-11-11", // insert the current date to access the latest version of the API
-      studioBasePath: "/studio", // If you want to access the Studio on a route
-    }),
-    react(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), sanity({
+    projectId: "jkn6q794",
+    dataset: "production",
+    useCdn: false, // See note on using the CDN
+    apiVersion: "2024-11-11", // insert the current date to access the latest version of the API
+    studioBasePath: "/studio", // If you want to access the Studio on a route
+  }), react(), icon()],
 });
