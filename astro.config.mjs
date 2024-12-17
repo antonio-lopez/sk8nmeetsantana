969703005b428b1d9147ffb18ce32bc795b386bc
antonio-lopez <36496887+antonio-lopez@users.noreply.github.com>
2024-12-17 07:57:12 -0800

    feat: add past meetups and configure astro for hybrid rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,16 +10,22 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "hybrid",
   image: {
     domains: ["res.cloudinary.com"],
   },
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  }), sanity({
-    projectId: "jkn6q794",
-    dataset: "production",
-    useCdn: false, // See note on using the CDN
-    apiVersion: "2024-11-11", // insert the current date to access the latest version of the API
-    studioBasePath: "/studio", // If you want to access the Studio on a route
-  }), react(), icon()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sanity({
+      projectId: "jkn6q794",
+      dataset: "production",
+      useCdn: false, // See note on using the CDN
+      apiVersion: "2024-11-11", // insert the current date to access the latest version of the API
+      studioBasePath: "/studio", // If you want to access the Studio on a route
+    }),
+    react(),
+    icon(),
+  ],
 });
