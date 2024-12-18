e363cc66a5780e5681bba036a3e861391a8e3759
antonio-lopez <36496887+antonio-lopez@users.noreply.github.com>
2024-12-17 16:44:50 -0800

    build: add astro netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,14 @@ import tailwind from "@astrojs/tailwind";
 
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
+import netlify from "@astrojs/netlify";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
+  adapter: netlify({ imageCDN: false }),
   image: {
     domains: ["res.cloudinary.com"],
   },
